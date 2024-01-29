53384a94f50af21e52eb603e81d612d980eb8e48
Melanie Archer <job@twobanjos.com>
2024-01-29 15:24:10 -0800

    19/19. Added combo comments.

--- hw2/tests.py
+++ hw2/tests.py
@@ -3,27 +3,44 @@ from contrived_func import contrived_func
 
 
 class TestContrivedFunc(unittest.TestCase):
+    ''''
+    Combo: F/T/T/F'''
     def test1(self):
         contrived_func(2)
+
+    ''''
+    Combo: F/F/T/T'''
 
     def test2(self):
         contrived_func(-1)
 
+    ''''
+    Combo: T/T/T/F'''
     def test3(self):
         contrived_func(14)
 
+    ''''
+    Combo: T/F/T/F'''
     def test4(self):
         contrived_func(15)
 
+    ''''
+    Combo: F/T/T/T'''
     def test5(self):
         contrived_func(-18)
 
+    ''''
+    Inputs: T/T/F/F'''
     def test6(self):
         contrived_func(26)
 
+    ''''
+    Inputs: F/F/T/F'''
     def test7(self):
         contrived_func(3)
 
+    ''''
+    Combo: T/F/F/F'''
     def test8(self):
         contrived_func(35)
 
