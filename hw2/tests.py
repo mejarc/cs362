1cb0563965c76593c3bcd4edbda3826a01212590
Melanie Archer <job@twobanjos.com>
2024-01-29 14:59:19 -0800

    18 of 19 points.

--- hw2/tests.py
+++ hw2/tests.py
@@ -10,16 +10,16 @@ class TestContrivedFunc(unittest.TestCase):
         contrived_func(-1)
 
     def test3(self):
-        contrived_func(10)
+        contrived_func(14)
 
     def test4(self):
         contrived_func(15)
 
     def test5(self):
-        contrived_func(20)
+        contrived_func(-18)
 
     def test6(self):
-        contrived_func(25)
+        contrived_func(26)
 
     def test7(self):
         contrived_func(30)
