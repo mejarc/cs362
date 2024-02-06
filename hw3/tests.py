45a1e81d5c3be7c888ef688bcf6952cd953675d1
Melanie Archer <job@twobanjos.com>
2024-02-05 17:43:54 -0800

    Still at 5/9

--- hw3/tests.py
+++ hw3/tests.py
@@ -13,7 +13,35 @@ def generate_credit_card_number(length):
 
 class TestCreditCardValidator(unittest.TestCase):
     def test1(self):
-        tests_to_generate = 5500
+        tests_to_generate = 270
+        for _ in range(tests_to_generate):
+            length = random.randint(10, 15)
+            credit_card_number = generate_credit_card_number(length)
+            credit_card_validator(credit_card_number)
+
+    def test2(self):
+        tests_to_generate = 330
+        for _ in range(tests_to_generate):
+            length = random.randint(10, 17)
+            credit_card_number = generate_credit_card_number(length)
+            credit_card_validator(credit_card_number)
+
+    def test3(self):
+        tests_to_generate = 450
+        for _ in range(tests_to_generate):
+            length = random.randint(15, 16)
+            credit_card_number = generate_credit_card_number(length)
+            credit_card_validator(credit_card_number)
+
+    def test4(self):
+        tests_to_generate = 330
+        for _ in range(tests_to_generate):
+            length = random.randint(14, 17)
+            credit_card_number = generate_credit_card_number(length)
+            credit_card_validator(credit_card_number)
+
+    def test5(self):
+        tests_to_generate = 330
         for _ in range(tests_to_generate):
             length = random.randint(9, 23)
             credit_card_number = generate_credit_card_number(length)
