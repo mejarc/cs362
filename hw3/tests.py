ff21d606008a0bd2f4df1046821e6d4657d6ae41
Melanie Archer <job@twobanjos.com>
2024-02-05 19:34:50 -0800

    6 of 9, so 5 bugs found

--- hw3/tests.py
+++ hw3/tests.py
@@ -4,6 +4,7 @@ from credit_card_validator import credit_card_validator
 
 
 def generate_credit_card_number(length):
+    random.seed()
     result = ''
     for _ in range(length):
         result += str(random.randint(0, 9))
@@ -13,37 +14,50 @@ def generate_credit_card_number(length):
 
 class TestCreditCardValidator(unittest.TestCase):
     def test1(self):
-        tests_to_generate = 270
+        tests_to_generate = 3250
         for _ in range(tests_to_generate):
-            length = random.randint(10, 15)
+            random.seed()
+            length = random.randint(10, 12)
             credit_card_number = generate_credit_card_number(length)
             credit_card_validator(credit_card_number)
 
     def test2(self):
-        tests_to_generate = 330
+        tests_to_generate = 3000
         for _ in range(tests_to_generate):
+            random.seed()
             length = random.randint(10, 17)
             credit_card_number = generate_credit_card_number(length)
             credit_card_validator(credit_card_number)
 
     def test3(self):
-        tests_to_generate = 450
+        tests_to_generate = 5500
         for _ in range(tests_to_generate):
+            random.seed()
             length = random.randint(15, 16)
             credit_card_number = generate_credit_card_number(length)
             credit_card_validator(credit_card_number)
 
     def test4(self):
-        tests_to_generate = 330
+        tests_to_generate = 5000
         for _ in range(tests_to_generate):
+            random.seed()
             length = random.randint(14, 17)
             credit_card_number = generate_credit_card_number(length)
             credit_card_validator(credit_card_number)
 
     def test5(self):
-        tests_to_generate = 330
+        tests_to_generate = 6000
         for _ in range(tests_to_generate):
+            random.seed()
             length = random.randint(9, 23)
+            credit_card_number = generate_credit_card_number(length)
+            credit_card_validator(credit_card_number)
+
+    def test6(self):
+        tests_to_generate = 3300
+        for _ in range(tests_to_generate):
+            random.seed()
+            length = random.randint(4, 16)
             credit_card_number = generate_credit_card_number(length)
             credit_card_validator(credit_card_number)
 
