4caa4a57806a0b09900cc8b214049b2b31a16286
Melanie Archer <job@twobanjos.com>
2024-02-06 14:18:13 -0800

    9 of 9. Time: Runtime: 24.81520438194275s

--- hw3/tests.py
+++ hw3/tests.py
@@ -13,7 +13,7 @@ def generate_credit_card_number(length):
 
 class TestCreditCardValidator(unittest.TestCase):
     def test1(self):
-        tests_to_generate = 900000
+        tests_to_generate = 805500
         for _ in range(tests_to_generate):
             length = random.randint(14, 17)
             credit_card_number = generate_credit_card_number(length)
