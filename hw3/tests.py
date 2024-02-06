d48fdd86a7ed3fb0c2311dd05d17cb2d915f67c1
Melanie Archer <job@twobanjos.com>
2024-02-05 17:23:07 -0800

    5 of 9.

--- hw3/tests.py
+++ hw3/tests.py
@@ -13,9 +13,9 @@ def generate_credit_card_number(length):
 
 class TestCreditCardValidator(unittest.TestCase):
     def test1(self):
-        tests_to_generate = 2333
+        tests_to_generate = 5500
         for _ in range(tests_to_generate):
-            length = random.randint(10, 20)
+            length = random.randint(9, 23)
             credit_card_number = generate_credit_card_number(length)
             credit_card_validator(credit_card_number)
 
